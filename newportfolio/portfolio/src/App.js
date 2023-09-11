d1b8ce87faad591b57eba0b4b25feca8a59e4501
Gustavo Martins <tioben@outlook.com.br>
2023-09-11 14:35:46 -0300

    11/09/2023

---
+++ newportfolio/portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//NavBar
+import Navbar from './components/Navbar.js'; //component
+import './styles/navstyle.css'; //style
+
+//proximo
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar logoname="Tio Ben" />
+    </div>
+  );
+}
+
+
